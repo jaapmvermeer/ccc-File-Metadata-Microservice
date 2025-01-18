16f9b3141556fafcc86449e3c94e0babffc4aaf2
Jaap Vermeer <jaapmvermeer@gmail.com>
2025-01-18 20:58:07 +0000

    Challenge Completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,8 +13,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.file.size, req.file.originalname);
+  res.json({name: req.file.originalname, type:req.file.mimetype ,size: req.file.size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
